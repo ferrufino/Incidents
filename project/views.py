878797445f834cf9be92650ae9fe44c24c5752bc
Eduardo Zardain <ejzardainc@gmail.com>
2014-05-02 20:51:37 -0500

    Adds simple query for manager view

--- project/views.py
+++ project/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.shortcuts import render, render_to_response
+from project.models import Incident, Account
+from django.db import connection
+
 
 # Create your views here.
 def index(request):
@@ -8,7 +12,9 @@ def index(request):
     return HttpResponse(template.render(context))
 
 def manager(request, adminid):
-    return HttpResponse("You're in admin view.")
+    template = loader.get_template('project/manager.html')
+    return render(request, "project/manager.html", {"table": Account.objects.raw("SELECT * FROM Account")})
+    #return HttpResponse("You're in admin view.")
 
 def employee(request, empid):
     return HttpResponse("You're in employees view.")
