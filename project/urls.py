1272e5b2b20e8665304e8e12a77bc4fb2278febc
Eduardo Zardain <ejzardainc@gmail.com>
2014-05-15 05:35:48 -0500

    adds url for registerTicket

--- project/urls.py
+++ project/urls.py
@@ -6,4 +6,7 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^manager/', views.manager),
     url(r'^employee/(?P<empid>\d+)', views.employee, name='employee'),
+    url(r'^registerTicket/', views.registerTicket),
+    url(r'^allTickets/', views.allTickets)
+
 )
