e297c862a08ede0bee4392c2a1dd84c163a0af26
Eduardo Zardain <ejzardainc@gmail.com>
2014-05-18 19:15:34 -0500

    all forms redirect and employee view

--- project/urls.py
+++ project/urls.py
@@ -8,7 +8,8 @@ urlpatterns = patterns('',
     url(r'^manager/(?P<adminid>\d+)', ManagerView.as_view(), name='Manager'),
     url(r'^employee/(?P<empid>\d+)', EmployeeView.as_view(), name='employee'),
     url(r'^registerTicket/(?P<adminid>\d)', RegisterTicket.as_view(), name='RegisterTicket'),
-    url(r'^AssignEmployee/', AssignEmployee.as_view(), name='AssignEmployee'),
+    url(r'^AssignEmployee/(?P<adminid>\d)', AssignEmployee.as_view(), name='AssignEmployee'),
+    url(r'^CloseIncident/(?P<adminid>\d)', RegisterTicket.as_view(), name='CloseIncident'),
     url(r'^allTickets/', views.allTickets)
 
 
